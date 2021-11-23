6c026c693276bb4d4628b8060a1d9387c7d7ef06
FernandoMos92 <fernandomosp@gmail.com>
2021-11-22 23:19:15 -0300

    feat: create Header and CardProduct

---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import sambay from '../../img/sambay.svg'
+
+class index extends Component {
+  render() {
+    return (
+      <div>
+        <img src={sambay} alt="sambay-icon" />
+      </div>
+    );
+  }
+}
+
+export default index;
