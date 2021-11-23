5d29170050a467dd03fed812f04ddf2d74cae021
FernandoMos92 <fernandomosp@gmail.com>
2021-11-23 00:14:53 -0300

    feat: Completed style for header

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
 import sambay from '../../img/sambay.svg'
 import { BsBell, BsCart3, BsSearch } from 'react-icons/bs';
+import "../../Style/Header.css";
 
 class index extends Component {
   render() {
     return (
       <div className="header-container">
-        <img src={sambay} alt="sambay-icon" />
-        <BsSearch />
+        <img 
+          src={sambay} 
+          alt="sambay-icon" 
+          className="icon-sambay"
+        />
+        <BsSearch className="icon-search"/>
         <input
+        className="search-input"
         type="text"
         placeholder="Busque aqui seu produto"
         />
-        <BsBell />
-        <BsCart3 />
+        <div className="user-container">
+          <BsBell className="icon-react"/>
+          <BsCart3 className="icon-react"/>
+          <div className="user-icon">
+
+          </div>
+        </div>
       </div>
     );
   }
