6850003682c7ef366dba4d0295b14495e0951150
FernandoMos92 <fernandomosp@gmail.com>
2021-11-22 23:29:32 -0300

    feat: imported icons from React-icons

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import sambay from '../../img/sambay.svg'
+import { BsBell, BsCart3, BsSearch } from 'react-icons/bs';
 
 class index extends Component {
   render() {
     return (
-      <div>
+      <div className="header-container">
         <img src={sambay} alt="sambay-icon" />
+        <BsSearch />
+        <input
+        type="text"
+        placeholder="Busque aqui seu produto"
+        />
+        <BsBell />
+        <BsCart3 />
       </div>
     );
   }
